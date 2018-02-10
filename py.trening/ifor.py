29233abe638bf2a95d28313f054f1288ff127ff7
uzekamanzi-pl <uze@gazeta.pl>
2018-02-10 15:28:34 +0100

    efsdfssdf

--- py.trening/ifor.py
+++ py.trening/ifor.py
@@ -3,12 +3,9 @@ number = input('podaj liczbe')
 if int(number) > 0:
     print('liczba jest wieksza od zera')
 
-elif int(number) == 0:
-    print('podana liczba to zero')
-
 elif int(number) < 0:
     print('podana liczba jest mniejsza od zera')
 
 else:
-    print('nie wprowadziłeś liczby')
+    print('podana liczba to zero')
 
